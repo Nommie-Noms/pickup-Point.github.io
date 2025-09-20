5e557abb6ed659691899595396e2e74be3df1d73
Nommie-Noms <chris.hcsjc6@gmail.com>
2025-09-20 21:22:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,17 +4,5 @@ function copyNumber(number) {
     alert("Phone number copied: " + number);
   });
 }
-
-// Mobile menu toggle
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('nav-links');
-
-hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('mobile');
-  if (navLinks.style.display === "flex") {
-    navLinks.style.display = "none";
-  } else {
-    navLinks.style.display = "flex";
-    navLinks.style.flexDirection = "column";
-  }
 });
+
